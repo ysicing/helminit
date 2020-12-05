89c968e63699bea3f97a342ecc65dba2fd7a6c2c
ysicing <i@ysicing.me>
2020-12-05 10:42:28 +0800

    feat: update nginx-ingress-controller
    Signed-off-by: ysicing <i@ysicing.me>

---
+++ get.nginx-ingress-controller.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+helm repo update 
+
+helm pull bitnami/nginx-ingress-controller --untar 
+
+kubectl create ns nginx-ingress
+
+helm upgrade -i nginx-ingress-controller -f nginx-ingress-controller/values.yaml -n nginx-ingress bitnami/nginx-ingress-controller --version 6.0.1
