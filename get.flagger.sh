01a66ad8dce55502710ffc9291b97e058bb0c853
ysicing <i@ysicing.me>
2021-01-04 23:22:18 +0800

    feat: update flagger
    Signed-off-by: ysicing <i@ysicing.me>

--- get.flagger.sh
+++ get.flagger.sh
@@ -6,4 +6,4 @@ helm pull flagger/flagger --untar
 
 kubectl create ns nginx-ingress
 
-helm upgrade -i flagger -f flagger/values.yaml flagger/flagger -n nginx-ingress --version 1.3.0
+helm upgrade -i flagger -f flagger/values.yaml flagger/flagger -n nginx-ingress --version 1.5.0
