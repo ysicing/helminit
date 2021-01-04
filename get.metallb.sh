0f2cdc2c02e8bc818d596949eba3e9cefae7f81f
ysicing <i@ysicing.me>
2021-01-04 23:01:14 +0800

    feat: update mlb
    Signed-off-by: ysicing <i@ysicing.me>

--- get.metallb.sh
+++ get.metallb.sh
@@ -6,4 +6,4 @@ helm pull  bitnami/metallb --untar
 
 kubectl create ns metallb-system
 
-helm upgrade -i metallb -f metallb/values.yaml -n metallb-system bitnami/metallb --version 1.0.1
+helm upgrade -i metallb -f metallb/values.yaml -n metallb-system bitnami/metallb --version 2.0.2
