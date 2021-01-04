a58beaaac7125dbd6ca60c2bdd8d654e5e2a52b9
ysicing <i@ysicing.me>
2021-01-04 23:17:04 +0800

    feat: update ingress
    Signed-off-by: ysicing <i@ysicing.me>

--- get.nginx-ingress-controller.sh
+++ get.nginx-ingress-controller.sh
@@ -6,4 +6,4 @@ helm pull bitnami/nginx-ingress-controller --untar
 
 kubectl create ns nginx-ingress
 
-helm upgrade -i nginx-ingress-controller -f nginx-ingress-controller/values.yaml -n nginx-ingress bitnami/nginx-ingress-controller --version 6.0.1
+helm upgrade -i nginx-ingress-controller -f nginx-ingress-controller/values.yaml -n nginx-ingress bitnami/nginx-ingress-controller --version 7.0.9
