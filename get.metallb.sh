93ca83f6d8113634947abd6ee0de5304c2e40ac8
ysicing <i@ysicing.me>
2021-02-03 16:32:48 +0800

    feat(metallb): release metallb 2.2.1
    release metallb 2.2.1
    
    Signed-off-by: ysicing <i@ysicing.me>

--- get.metallb.sh
+++ get.metallb.sh
@@ -2,8 +2,8 @@
 
 helm repo update
 
-helm pull  bitnami/metallb --untar
+# helm pull bitnami/metallb --untar
 
 kubectl create ns metallb-system
 
-helm upgrade -i metallb -f metallb/values.yaml -n metallb-system bitnami/metallb --version 2.0.2
+helm upgrade -i metallb -f metallb/values.yaml -n metallb-system bitnami/metallb --version 2.2.1
